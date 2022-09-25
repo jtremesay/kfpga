71c6777cdbdb9005a7e3babf7dbc4c42cc5e1b35
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-09-25 18:39:05 +0200

    remae core, creat, smaller core for test purpose

--- kfpga/__init__.py
+++ kfpga/__init__.py
@@ -3,13 +3,22 @@ from .hdl.kfpga import create_kfpga_library
 
 
 def main():
-    library = create_kfpga_library()
 
     base_dir = Path(__file__).parent
 
-    output_dir = base_dir.parent / "rtl" / "gen"
+    output_dir = base_dir.parent / "rtl" / "k1g8x8y4io10ic4c6l"
     output_dir.mkdir(exist_ok=True, parents=True)
-    # for entry in output_dir.iterdir():
-    #    print(entry)
+    library = create_kfpga_library()
+    library.write_verilog_dir(output_dir)
 
+    output_dir = base_dir.parent / "rtl" / "k1g3x3y4io6ic2c4l"
+    output_dir.mkdir(exist_ok=True, parents=True)
+    library = create_kfpga_library(
+        width=3,
+        height=3,
+        io_pairs_count=4,
+        interconnect_pairs_count=6,
+        cluster_size=2,
+        lut_size=4,
+    )
     library.write_verilog_dir(output_dir)
