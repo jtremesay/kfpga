51a57d97e2916c7680b25c5c560e820d2b829f15
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-10-22 00:48:32 +0200

    simplify shift register

--- kfpga/templates/verilog/base/ShiftRegister.v
+++ kfpga/templates/verilog/base/ShiftRegister.v
@@ -1,15 +1,13 @@
 {% extends "verilog/base/Module.v" %}
 
 {% block body %}
-    reg [{{ module.width - 1}}:0] r_data;
+    reg [{{ module.width - 1 }}:0] r_data;
     always @(posedge clock) begin
         if (!nreset) begin
             r_data <= 0;
         end else begin
             if (enable) begin
-                {% for i in range(module.width) %}
-                r_data[{{ i }}] <= {% if i == 0 %}data_in{% else %}r_data[{{ i - 1 }}]{% endif %};
-                {% endfor %}
+                r_data <= {r_data[{{ module.width - 2}}:0], data_in};
             end
         end
     end
