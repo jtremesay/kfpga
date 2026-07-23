4ee64c0cb3fe04050611e6fd11eeb6f0842a5676
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-07-24 01:53:58 +0200

    feat: add LogicTile and SwitchBox components with configuration layouts

---
+++ src/kfpga/components/lt.py
@@ -0,0 +1,113 @@
+import logging
+from dataclasses import dataclass
+
+from amaranth.lib.data import StructLayout
+from amaranth.lib.wiring import Component, In, Module, Out, Signal
+
+from ..consts import SideFlag
+from .lv import LogicVector, LogicVectorConfig, LogicVectorConfigLayout
+from .sb import SwitchBox, SwitchBoxConfig, SwitchBoxConfigLayout
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class LogicTileConfig:
+    lv: LogicVectorConfig
+    sb: SwitchBoxConfig
+
+
+class LogicTileConfigLayout(StructLayout):
+    def __init__(
+        self,
+        io_size: int,
+        ic_size: int,
+        io_sides: SideFlag,
+        vector_size: int,
+        lut_size: int,
+    ) -> None:
+        self.vector_size = vector_size
+        self.lut_size = lut_size
+        super().__init__(
+            {
+                "lv": LogicVectorConfigLayout(vector_size, lut_size),
+                "sb": SwitchBoxConfigLayout(
+                    io_size, ic_size, io_sides, vector_size, lut_size
+                ),
+            }
+        )
+
+
+class LogicTile(Component):
+    def __init__(
+        self,
+        io_size: int,
+        ic_size: int,
+        io_sides: SideFlag,
+        vector_size: int,
+        lut_size: int,
+    ) -> None:
+        self.io_size = io_size
+        self.ic_size = ic_size
+        self.io_sides = io_sides
+        self.vector_size = vector_size
+        self.lut_size = lut_size
+        self.config_layout = LogicTileConfigLayout(
+            io_size, ic_size, io_sides, vector_size, lut_size
+        )
+
+        super().__init__(
+            {
+                "data_north_in": In(io_size if SideFlag.NORTH in io_sides else ic_size),
+                "data_north_out": Out(
+                    io_size if SideFlag.NORTH in io_sides else ic_size
+                ),
+                "data_east_in": In(io_size if SideFlag.EAST in io_sides else ic_size),
+                "data_east_out": Out(io_size if SideFlag.EAST in io_sides else ic_size),
+                "data_south_in": In(io_size if SideFlag.SOUTH in io_sides else ic_size),
+                "data_south_out": Out(
+                    io_size if SideFlag.SOUTH in io_sides else ic_size
+                ),
+                "data_west_in": In(io_size if SideFlag.WEST in io_sides else ic_size),
+                "data_west_out": Out(io_size if SideFlag.WEST in io_sides else ic_size),
+                "config": In(self.config_layout),
+            }
+        )
+        self.data_north_in: Signal = self.data_north_in
+        self.data_north_out: Signal = self.data_north_out
+        self.data_east_in: Signal = self.data_east_in
+        self.data_east_out: Signal = self.data_east_out
+        self.data_south_in: Signal = self.data_south_in
+        self.data_south_out: Signal = self.data_south_out
+        self.data_west_in: Signal = self.data_west_in
+        self.data_west_out: Signal = self.data_west_out
+        self.config = self.config_layout(self.config)
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        m.submodules.lv = lv = LogicVector(self.vector_size, self.lut_size)
+        m.submodules.sb = sb = SwitchBox(
+            self.io_size,
+            self.ic_size,
+            self.io_sides,
+            self.vector_size,
+            self.lut_size,
+        )
+        m.d.comb += [
+            sb.data_north_in.eq(self.data_north_in),
+            sb.data_north_out.eq(self.data_north_out),
+            sb.data_east_in.eq(self.data_east_in),
+            sb.data_east_out.eq(self.data_east_out),
+            sb.data_south_in.eq(self.data_south_in),
+            sb.data_south_out.eq(self.data_south_out),
+            sb.data_west_in.eq(self.data_west_in),
+            sb.data_west_out.eq(self.data_west_out),
+            sb.data_lv_out.eq(lv.data_out),
+            sb.data_lv_in.eq(lv.data_in),
+            sb.config.eq(self.config.sb),
+            lv.data_in.eq(sb.data_east_in),
+            lv.config.eq(self.config.lv),
+        ]
+
+        return m
---
+++ src/kfpga/components/sb.py
@@ -0,0 +1,81 @@
+import logging
+from dataclasses import dataclass
+
+from amaranth.lib.data import StructLayout
+from amaranth.lib.wiring import Component, In, Out, Signal
+
+from ..consts import SideFlag
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class SwitchBoxConfig:
+    pass
+
+
+class SwitchBoxConfigLayout(StructLayout):
+    def __init__(
+        self,
+        io_size: int,
+        ic_size: int,
+        io_sides: SideFlag,
+        vector_size: int,
+        lut_size: int,
+    ) -> None:
+        self.io_size = io_size
+        self.ic_size = ic_size
+        self.io_sides = io_sides
+        self.vector_size = vector_size
+        self.lut_size = lut_size
+        super().__init__({})
+
+
+class SwitchBox(Component):
+    def __init__(
+        self,
+        io_size: int,
+        ic_size: int,
+        io_sides: SideFlag,
+        vector_size: int,
+        lut_size: int,
+    ) -> None:
+        self.io_size = io_size
+        self.ic_size = ic_size
+        self.io_sides = io_sides
+        self.vector_size = vector_size
+        self.lut_size = lut_size
+        self.config_layout = SwitchBoxConfigLayout(
+            io_size, ic_size, io_sides, vector_size, lut_size
+        )
+
+        super().__init__(
+            {
+                "data_north_in": In(io_size if SideFlag.NORTH in io_sides else ic_size),
+                "data_north_out": Out(
+                    io_size if SideFlag.NORTH in io_sides else ic_size
+                ),
+                "data_east_in": In(io_size if SideFlag.EAST in io_sides else ic_size),
+                "data_east_out": Out(io_size if SideFlag.EAST in io_sides else ic_size),
+                "data_south_in": In(io_size if SideFlag.SOUTH in io_sides else ic_size),
+                "data_south_out": Out(
+                    io_size if SideFlag.SOUTH in io_sides else ic_size
+                ),
+                "data_west_in": In(io_size if SideFlag.WEST in io_sides else ic_size),
+                "data_west_out": Out(io_size if SideFlag.WEST in io_sides else ic_size),
+                "data_lv_in": Out(vector_size * lut_size),
+                "data_lv_out": In(vector_size),
+                "config": In(self.config_layout),
+            }
+        )
+        self.data_north_in: Signal = self.data_north_in
+        self.data_north_out: Signal = self.data_north_out
+        self.data_east_in: Signal = self.data_east_in
+        self.data_east_out: Signal = self.data_east_out
+        self.data_south_in: Signal = self.data_south_in
+        self.data_south_out: Signal = self.data_south_out
+        self.data_west_in: Signal = self.data_west_in
+        self.data_west_out: Signal = self.data_west_out
+        self.data_lv_in: Signal = self.data_lv_in
+        self.data_lv_out: Signal = self.data_lv_out
+        self.config = self.config_layout(self.config)
